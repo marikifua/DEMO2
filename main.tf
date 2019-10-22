36381ed7909ade3e61395f33dee9bd68cfbaca31
marikifua <marik_if_ua@ukr.net>
2019-10-22 13:39:25 +0300

    update

--- main.tf
+++ main.tf
@@ -5,8 +5,8 @@ provider "google" {
   region  = "europe-west3"
   zone    = "europe-west3-c"
 }
-resource "google_compute_instance" "carts" {
-	name         = "carts"
+resource "google_compute_instance" "carts-tf" {
+	name         = "carts-tf"
 	machine_type = "g1-small"
 
 
@@ -26,7 +26,7 @@ resource "google_compute_instance" "carts" {
 	}
   metadata = { ssh-keys = "${var.ssh_user}:${file(var.ssh_pub_key ["${var.ssh_user}"])}" }
     
-    tags = [ "carts" ]
+    tags = [ "carts-tf" ]
 }
 resource "google_compute_instance" "mongodb-tf" {
 	name         = "mongodb-tf"
